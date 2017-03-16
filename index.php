5babd253b52651de06e9060125d9e6b1da64a69a
root <root@ursula.noc.uowm.gr>
2017-03-16 14:08:45 +0200

    Changed start page table

--- index.php
+++ index.php
@@ -12,11 +12,16 @@
 			<td><strong>Salt</strong></td>
 			<td><?php echo $salt; ?></td>
 		</tr>
+		<tr>
+			<td><strong>Allowed ip addresses</strong></td>
+			<td><?php foreach ($allowedips as &$ip) { echo $ip."<br>"; } ?></td>
+		</tr>
+		<tr>
+			<td><strong>Your ip address</strong></td>
+			<td><?php echo $remoteip; ?></td>
+		</tr>
 	</tbody>
 </table>
 
 
-
-
 <?php require('footer.inc.php'); ?>
-            
